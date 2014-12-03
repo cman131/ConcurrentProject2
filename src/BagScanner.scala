d19cdc16c67de13f7707dbf3e4e2a97d05b61bb0
cman131 <cgw5994@rit.edu>
2014-12-03 13:23:13 -0500

    layed out the scanners

--- src/BagScanner.scala
+++ src/BagScanner.scala
@@ -1,6 +1,6 @@
 /**
  * Created by Conor on 12/3/2014.
- * The Body scanner in the airport
+ * The Bag scanner in the airport
  */
 
 import akka.actor.Actor
@@ -8,7 +8,7 @@ sealed abstract class Message
 case object Setup extends Message
 case object SendPassenger extends Message
 
-class BodyScanner extends Actor[Message] {
+class BagScanner extends Actor[Message] {
 	def receive = {
 		case Setup => () // receive setup msg
 		case SendPassenger => () // receive a new passenger
