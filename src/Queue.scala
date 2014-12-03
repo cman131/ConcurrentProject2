8d900550835fc438d5fd7e26973aae1b2697553f
cman131 <cgw5994@rit.edu>
2014-12-03 13:46:52 -0500

    worked further on queue

--- src/Queue.scala
+++ src/Queue.scala
@@ -4,15 +4,40 @@
  */
 
 import akka.actor.Actor
+import scala.collection.immutable.Queue
+
 sealed abstract class Message
 case object Setup extends Message
 case object SendPassenger extends Message
 case object Notify extends Message
 
 class Queue extends Actor[Message] {
+	val body
+	val bag
+	val isBagFree = True
+	val isBodyFree = True
+
+	val bodies = Queue.empty[Passenger]
+	val bags = Queue.empty[Passenger]
+
 	def receive = {
-		case Setup => () // receive setup msg
-		case SendPassenger => () // receive a new passenger
-		case Notify => () // a scanner has become free
+		case Setup(msg) => // receive setup msg
+			body = msg.bodyRef
+			bag = msg.bagRef
+		case SendPassenger(msg) => () // receive a new passenger
+			bodies = msg.passenger +: bodies
+			bags = msg.passenger +: bags
+			// Now update the scanners if they are free
+
+		case Notify(msg) => // a scanner has become free
+			{
+			case BodyScanner =>
+				//TODO
+			case BagScanner =>
+				//TODO
+			default =>
+				// I've been poisoned
+			}
+
 	}
 }
