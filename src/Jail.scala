7757f9eccc727d75f6640232dc93827f975c80e5
kocsen <kocsenc@gmail.com>
2014-12-08 10:20:11 -0500

    Jail

--- src/Jail.scala
+++ src/Jail.scala
@@ -1,16 +1,23 @@
 import akka.actor.Actor
-import akka.actor.Props
 import akka.event.Logging
 
 /**
  * Kocsen Chung
  * Jail, receives all criminals
  */
+case class sendPassenger(pass: Passenger)
 
 class Jail extends Actor {
+  var captives = List()
+
+
   val log = Logging(context.system, this)
+
   def receive = {
-    case "test" => log.info("received test")
-    case _      => log.info("received unknown message")
+    case sendPassenger(passenger) =>
+      captives :+ passenger
+      log.info("received passenger")
+
+    case _ => print("received unknown message")
   }
 }
