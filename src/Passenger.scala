0473b5181c5830d63602a8ca0be5bc286c2c6ebc
kocsen <kocsenc@gmail.com>
2014-12-08 13:45:54 -0500

    passenger optimizations

--- src/Passenger.scala
+++ src/Passenger.scala
@@ -1,25 +1,23 @@
 /**
  * Passenger.scala
- * 
+ *
  * Simple class to represent a passenger who owns one bag.
- * 
+ *
  * @author Geoff Berl
  */
 
-import akka.actor.Actor
-
 class Passenger {
-  
+
   // Some fields maybe??
-  var hasBag : Boolean = true;
-  
+  var hasBag: Boolean = true
+
   // Some functions or getters???
-  def getHasBag() : Boolean = {
-    return this.hasBag;
+  def getHasBag: Boolean = {
+    this.hasBag
   }
-  
-  def setHasBag(b : Boolean) {
-    this.hasBag = b;
+
+  def setHasBag(b: Boolean) {
+    this.hasBag = b
   }
-  
+
 }
