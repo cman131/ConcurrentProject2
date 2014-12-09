9a0c186a16306d38237dee408314cb210ded3a73
kocsen <kocsenc@gmail.com>
2014-12-09 12:54:01 -0500

    Main

--- src/driver/Main.scala
+++ src/driver/Main.scala
@@ -4,6 +4,8 @@ import actors._
 import akka.actor.{ActorSystem, Props}
 import messages.Setup
 
+import scala.collection.mutable.ListBuffer
+
 /**
  * Created by Conor on 11/20/2014.
  * main entry point.
@@ -13,6 +15,7 @@ object Main {
   def main(args: Array[String]) {
     val NUM_PASSENGERS: Int = 4
     val NUM_LINES: Int = 4
+
 
     /**
      * Actors to start
@@ -24,6 +27,9 @@ object Main {
      * - Queue Actor
      * -
      */
+
+    val lineQueues = new ListBuffer[LineQueue]
+
     val system = ActorSystem("mySystem")
 
     val jailActor = system.actorOf(Props[actors.Jail])
@@ -32,17 +38,19 @@ object Main {
     for (a <- 1 to NUM_LINES) {
       println("Creating line: " + a)
       val securityActor = system.actorOf(Props(classOf[Security], jailActor, systemActor, a))
-      val bagScannerActor = system.actorOf(Props[BagScanner]) // need messages.Setup msg
-      val bodyScannerActor = system.actorOf(Props[BodyScanner]) // need messages.Setup msg
-      val queueActor = system.actorOf(Props[LineQueue]) // need messages.Setup msg
-      val setupMsg = Setup(queueActor, securityActor, bodyScannerActor, bagScannerActor)
+      val bagScannerActor = system.actorOf(Props[BagScanner])
+      val bodyScannerActor = system.actorOf(Props[BodyScanner])
+      val queueActor = system.actorOf(Props[LineQueue])
+      val setupMsg = Setup(queueActor, securityActor, bodyScannerActor, bagScannerActor, a)
+
+      lineQueues :+ queueActor
 
       bagScannerActor ! setupMsg
       bodyScannerActor ! setupMsg
       bodyScannerActor ! setupMsg
     }
 
-    val documentCheckActor = system.actorOf(Props[DocumentCheck]) // need messages.Setup msg
+    val documentCheckActor = system.actorOf(Props(classOf[DocumentCheck], lineQueues))
 
 
 
