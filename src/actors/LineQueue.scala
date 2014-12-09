51e62c966c2a3c65b4d73c709eea5094fca7b89f
Conor Wright <conor13@msn.com>
2014-12-09 13:00:05 -0500

    added print statements to queue and scanners

--- src/actors/LineQueue.scala
+++ src/actors/LineQueue.scala
@@ -31,29 +31,38 @@ class LineQueue extends Actor {
 			line = msg.getLine()
 			println("Queue #"+line+" is set up.")
 		case msg: SendPassenger => // receive a new passenger
+			println("Queue #"+line+" has received a new passenger.")
 			bodies += msg.passenger //+: bodies
 			bags += msg.passenger //+: bags
 			// Now update the scanners if they are free
 			if(isBagFree){
+				println("Queue #"+line+" sends passenger #"+msg.passenger.getId()+" to Bag Scanner #"+line+".")
 				bag ! new SendPassenger(bags.dequeue())
 				isBagFree = false
 			}
 			if(isBodyFree){
+				println("Queue #"+line+" sends passenger #"+msg.passenger.getId()+" to Body Scanner #"+line+".")
 				body ! new SendPassenger(bodies.dequeue())
 				isBodyFree = false
 			}
 
 		case msg: Notify => // a scanner has become free
 			case b: BodyScanner =>
+				println("Queue #"+line+" has been notified that Body Scanner #"+line+"is free.")
 				if(bodies.size>0){
-					body ! new SendPassenger(bodies.dequeue())
+					val pass: Passenger = bodies.dequeue()
+					println("Queue #"+line+" sends passenger #"+pass+" to Body Scanner #"+line+".")
+					body ! new SendPassenger(pass)
 				}
 				else{
 					isBodyFree = true;
 				}
 			case b: BagScanner =>
+				println("Queue #"+line+" has been notified that Bag Scanner #"+line+"is free.")
 				if(bags.size>0){
-					bag ! new SendPassenger(bags.dequeue())
+					val pass: Passenger = bags.dequeue()
+					println("Queue #"+line+" sends passenger #"+pass+" to Bag Scanner #"+line+".")
+					bag ! new SendPassenger(pass)
 				}
 				else{
 					isBagFree = true;
