4ab81bda064880496b87ce213e12634f9bc20e77
cman131 <cgw5994@rit.edu>
2014-12-09 15:32:45 -0500

    updated prints in bodyscanner

--- src/actors/BodyScanner.scala
+++ src/actors/BodyScanner.scala
@@ -16,21 +16,21 @@ class BodyScanner extends Actor {
 
 	def receive = {
 		case msg: PoisonPill =>
-			println("Body Scanner #"+line+" has been poisoned! Shutting down.")
-			println("Body Scanner #"+line+" is now poisoning Security #"+line+".")
+			println("Body Scanner #"+line+": PoisonPill -> Shutting down.")
+			println("Body Scanner #"+line+": is now poisoning Security #"+line+".")
 			security ! new PoisonPill(true)
 			context.stop(self)
 		case msg: Setup => // receive setup msg
 			queue = msg.getQueue()
 			security = msg.getSecurity()
 			line = msg.getLine()
-			println("Body Scanner #"+line+" is set up.")
+			println("Body Scanner #"+line+": is set up.")
 		case msg: SendPassenger => // receive a new passenger
-			println("Body Scanner #"+line+" has received a new passenger.")
+			println("Body Scanner #"+line+": has received passenger #"+msg.passenger.getId()+".")
 			val result: Boolean = (new util.Random).nextInt(5)!=2
-			println("Body Scanner #"+line+" sends passenger #"+msg.passenger.getId()+" to security.")
+			println("Body Scanner #"+line+": sends passenger #"+msg.passenger.getId()+" to security.")
 			security ! new SendPassenger(msg.passenger, result, true)
-			println("Body Scanner #"+line+" notifies queue #"+line+" that it's empty.")
+			println("Body Scanner #"+line+": notifies queue #"+line+" that it's empty.")
 			queue ! new Notify(true)
 	}
 }
